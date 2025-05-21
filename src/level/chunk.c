14de4c88eaefc47857d616129b94d2161669b965
SuperStik <daviswood290@gmail.com>
2025-05-20 20:35:12 -0700

    Add chunk

---
+++ src/level/chunk.c
@@ -0,0 +1,30 @@
+#include <OpenGL/gl.h>
+
+#include "chunk.h"
+
+int UBLC_chunk_rebuilt_this_frame = 0;
+unsigned UBLC_chunk_updates = 0;
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
+struct UBLC_chunk *UBLC_chunk_init(struct UBLC_chunk *chunk, int x_lo, int y_lo,
+		int z_lo, int x_hi, int y_hi, int z_hi) {
+	UBLC_AABB_INIT(&(chunk->aabb), (float)x_lo, (float)y_lo, (float)z_lo,
+			(float)x_hi, (float)y_hi, (float)z_hi);
+	chunk->x_lo = x_lo;
+	chunk->y_lo = y_lo;
+	chunk->z_lo = z_lo;
+	chunk->x_hi = x_hi;
+	chunk->y_hi = y_hi;
+	chunk->z_hi = z_hi;
+	chunk->_lists = glGenLists(2);
+	chunk->_dirty = 1;
+	return chunk;
+}
+
+#pragma GCC diagnostic pop
+
+void UBLC_chunk_setdirty(struct UBLC_chunk *chunk) {
+	chunk->_dirty = 1;
+}
---
+++ src/level/chunk.h
@@ -0,0 +1,26 @@
+#ifndef UBLC_LEVEL_CHUNK_H
+#define UBLC_LEVEL_CHUNK_H 1
+
+#include <phys/AABB.h>
+
+struct UBLC_chunk {
+	struct UBLC_AABB aabb;
+	int x_lo;
+	int y_lo;
+	int z_lo;
+	int x_hi;
+	int y_hi;
+	int z_hi;
+	unsigned _lists;
+	unsigned char _dirty:1;
+};
+
+extern int UBLC_chunk_rebuilt_this_frame;
+extern unsigned UBLC_chunk_updates;
+
+struct UBLC_chunk *UBLC_chunk_init(struct UBLC_chunk *, int x_lo, int y_lo, int
+		z_lo, int x_hi, int y_hi, int z_hi);
+
+void UBLC_chunk_setdirty(struct UBLC_chunk *);
+
+#endif /* UBLC_LEVEL_CHUNK_H */
