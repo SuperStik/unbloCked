5aacf656ed94c40ea58683b2421c3eb9ab24c19f
SuperStik <daviswood290@gmail.com>
2025-05-20 21:19:11 -0700

    Add tesselator

---
+++ src/level/tesselator.c
@@ -0,0 +1,29 @@
+#include "tesselator.h"
+
+#define MAX_VERTICES 100000
+
+static size_t vertices = 0;
+
+static float vertexbuffer[MAX_VERTICES * 3];
+static float texcoordbuffer[MAX_VERTICES * 2];
+static float colorbuffer[MAX_VERTICES * 3];
+
+void UBLC_tesselator_flush(void) {
+	vertices = 0;
+}
+
+void UBLC_tesselator_vertex(struct UBLC_vertex *vertex) {
+	vertexbuffer[vertices * 3 + 0] = vertex->x;
+	vertexbuffer[vertices * 3 + 1] = vertex->y;
+	vertexbuffer[vertices * 3 + 2] = vertex->z;
+
+	texcoordbuffer[vertices * 2 + 0] = vertex->u;
+	texcoordbuffer[vertices * 2 + 1] = vertex->v;
+
+	colorbuffer[vertices * 3 + 0] = vertex->r;
+	colorbuffer[vertices * 3 + 1] = vertex->g;
+	colorbuffer[vertices * 3 + 2] = vertex->b;
+
+	if (++vertices == MAX_VERTICES)
+		UBLC_tesselator_flush();
+}
---
+++ src/level/tesselator.h
@@ -0,0 +1,18 @@
+#ifndef UBLC_LEVEL_TESSELATOR_H
+#define UBLC_LEVEL_TESSELATOR_H 1
+
+struct UBLC_vertex {
+	float x;
+	float y;
+	float z;
+	float u;
+	float v;
+	float r;
+	float g;
+	float b;
+}
+
+void UBLC_tesselator_flush(void);
+void UBLC_tesselator_vertex(struct UBLC_vertex *);
+
+#endif /* UBLC_LEVEL_TESSELATOR_H */
