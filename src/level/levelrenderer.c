043c615ad0eb591ab7afb2948a0d55dd4cdde5ad
SuperStik <daviswood290@gmail.com>
2025-05-22 15:26:52 -0700

    More level renderer functions

--- src/level/levelrenderer.c
+++ src/level/levelrenderer.c
@@ -1,9 +1,15 @@
 #include <err.h>
 #include <stdlib.h>
 
+#include <OpenGL/gl.h>
+
 #include "chunk.h"
+#include "frustum.h"
 #include "levelrenderer.h"
 #include "level.h"
+#include <phys/AABB.h>
+#include "tesselator.h"
+#include "tile.h"
 
 #define CHUNK_SIZE 16
 
@@ -55,3 +61,70 @@ void UBLC_levelrenderer_delete(void) {
 	free(chunks);
 	chunks = NULL;
 }
+
+void UBLC_levelrenderer_render(struct UBLC_player *player, int layer) {
+	UBLC_chunk_rebuilt_this_frame = 0;
+
+	gvec(float,4) frustum[6];
+	UBLC_frustum_get(frustum);
+
+	size_t totalchunks = (size_t)xchunks * ychunks * zchunks;
+	for (size_t i = 0; i < totalchunks; ++i) {
+		struct UBLC_AABB aabb = chunks[i].aabb;
+		if (UBLC_frustum_hascube(frustum, aabb.x_lo, aabb.y_lo,
+					aabb.z_lo, aabb.x_hi, aabb.y_hi,
+					aabb.z_hi))
+			UBLC_chunk_render(chunks + i, layer);
+	}
+}
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+
+void UBLC_levelrenderer_pick(struct UBLC_player *player) {
+	float r = 3.0f;
+	struct UBLC_AABB box = player->aabb;
+	UBLC_AABB_grow(&box, r, r, r);
+	int xlo = (int)box.x_lo;
+	int xhi = (int)(box.x_hi + 1.0f);
+	int ylo = (int)box.y_lo;
+	int yhi = (int)(box.y_hi + 1.0f);
+	int zlo = (int)box.z_lo;
+	int zhi = (int)(box.z_hi + 1.0f);
+	glInitNames();
+
+	for (int x = xlo; x < xhi; ++x) {
+		glPushName(x);
+
+		for (int y = ylo; y < yhi; ++y) {
+			glPushName(y);
+
+			for (int z = zlo; z < zhi; ++z) {
+				glPushName(z);
+
+				if (!UBLC_level_issolid(x, y, z)) {
+					glPopName();
+					continue;
+				}
+
+				for (int i = 0; i < 6; ++i) {
+					glPushName(i);
+
+					UBLC_tesselator_init();
+					UBLC_tile_renderface(x, y, z, i);
+					UBLC_tesselator_flush();
+
+					glPopName();
+				}
+
+				glPopName();
+			}
+
+			glPopName();
+		}
+
+		glPopName();
+	}
+}
+
+#pragma GCC diagnostic pop
--- src/level/levelrenderer.h
+++ src/level/levelrenderer.h
@@ -1,7 +1,11 @@
 #ifndef UBLC_LEVEL_LEVELRENDERER
 #define UBLC_LEVEL_LEVELRENDERER 1
 
+#include <player.h>
+
 void UBLC_levelrenderer_init(void);
 void UBLC_levelrenderer_delete(void);
+
+void UBLC_levelrenderer_render(struct UBLC_player *, int layer);
 
 #endif /* UBLC_LEVEL_LEVELRENDERER */
