6e9144b04a633c339e0bf6e66f92fa2972ef965e
SuperStik <daviswood290@gmail.com>
2025-06-01 12:04:05 -0700

    Use SDL and POSIX-2008 functions to get resources descriptor

--- src/resources.c
+++ src/resources.c
@@ -1,31 +1,30 @@
 #include <err.h>
-#include <errno.h>
 #include <fcntl.h>
-#include <libgen.h>
-#include <limits.h>
-#include <mach-o/dyld.h>
-#include <string.h>
 #include <unistd.h>
+
+#include <SDL3/SDL_filesystem.h>
 
 #include "resources.h"
 
 int UBLC_resource_fd = -1;
 
 __attribute__((constructor)) static void resource_init(void) {
-	char execpath[PATH_MAX];
-	uint32_t buflen = PATH_MAX;
-	if (_NSGetExecutablePath(execpath, &buflen))
-		errc(2, ENOMEM, "_NSGetExecutablePath");
+	const char *path = SDL_GetBasePath();
+	if (path == NULL)
+		errx(2, "%s", SDL_GetError());
 
-	char path[PATH_MAX];
-	dirname_r(execpath, path);
-	strlcat(path, "/resources", PATH_MAX);
-
-	int res_fd = open(path, O_SEARCH | O_DIRECTORY);
-	if (res_fd < 0)
+	int base = open(path, O_SEARCH | O_DIRECTORY);
+	if (base < 0)
 		err(2, "open: %s", path);
 
+	int res_fd = openat(base, "resources", O_SEARCH | O_DIRECTORY);
+	if (res_fd < 0)
+		err(2, "openat: %i %s", base, path);
+
 	UBLC_resource_fd = res_fd;
+
+	if (close(base))
+		warn("close: %i", base);
 }
 
 /* fds are closed on exit */
